a38a31dd272faceddf4aa7cfbe018cd883fbe7fa
arellanoelden <arellanoelden11@yahoo.com>
2019-07-10 16:54:49 -0700

    redux update

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,24 +1,49 @@
 import React from "react";
 import { Link } from "@reach/router";
 import styled from "@emotion/styled";
+import colors from "./colors";
+
+// const Spin = keyframes`
+//   from {
+//     transform: rotate(0deg)
+//   }
+//   to {
+//     transform: rotate(360deg)
+//   }
+// `;
+
+// const SpyGlass = styled("span")`
+//   display: inline-block;
+//   animation: 1s ${Spin};
+// `;
 
 const Container = styled("header")`
-  background-color: #333;
+  background-color: ${colors.dark};
   position: sticky;
   top: 0;
   z-index: 10;
 `;
 
+const NavLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+  span {
+    display: inline-block;
+    border: 1px solid red;
+    color: ${colors.secondary};
+  }
+`;
 class NavBar extends React.Component {
   render() {
     return (
       <Container>
-        <Link to="/">Adopt Me!</Link>
-        <Link to="search-params">
+        <NavLink to="/">Adopt Me!</NavLink>
+        <NavLink to="search-params">
           <span aria-label="search" role="img">
             Search
           </span>
-        </Link>
+        </NavLink>
       </Container>
     );
   }
