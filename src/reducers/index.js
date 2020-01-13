8c28b04b8ba3716e7271a621e954e4521fe38912
arellanoelden <arellanoelden11@yahoo.com>
2020-01-12 17:46:06 -0800

    add folders for redux

---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+import { combineReducers } from "redux";
+import location from "./location";
+import animal from "./animal";
+import breed from "./breed";
+import breeds from "./breeds";
+
+/*
+  state = { location: "Seatlle, Wa" }
+  action = { type: "SET_LOCATION", payload: "San Francisco, CA"}
+*/
+
+// const rootReducer = function(state, action) {
+//   switch (action.type) {
+//     case "SET_LOCATION":
+//       return location(state, action);
+//   }
+// };
+
+export default combineReducers({
+  location,
+  animal,
+  breed,
+  breeds
+});
